db345a09706c63dbdb02ce5c853ea0a5d48264a9
Ruth Ramirez <ruthralcazar@gmail.com>
2026-04-06 00:40:46 -0700

    HW3 Express App

---
+++ index.mjs
@@ -0,0 +1,65 @@
+import express from 'express';
+import catFacts from 'cat-facts';
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get('/', async (req, res) => {
+    const id = Math.floor(Math.random() * 826) + 1;
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await response.json();
+
+    res.render("home.ejs", { character: data });
+});
+
+app.get('/search', async (req, res) => {
+   res.render('search.ejs')
+});
+
+app.get('/species', async (req, res) => {
+    let species = req.query.species || 'human';
+
+    let response = await fetch(`https://rickandmortyapi.com/api/character/?species=${species}`);
+    let data = await response.json();
+
+    res.render('species.ejs', { characters: data.results, species });
+});
+
+
+app.get('/searchName', async (req, res) => {
+   let name = req.query.name;
+   let url = `https://rickandmortyapi.com/api/character/?name=${name}`;
+   let response = await fetch(url);
+   let data = await response.json();
+
+   res.render('results.ejs', { characters: data.results });
+});
+
+app.get('/searchStatus', async (req, res) => {
+   let status = req.query.status;
+   let url = `https://rickandmortyapi.com/api/character/?status=${status}`;
+   let response = await fetch(url);
+   let data = await response.json();
+
+   res.render('results.ejs', { characters: data.results });
+});
+
+app.get('/searchGender', async (req, res) => {
+   let gender = req.query.gender;
+   let url = `https://rickandmortyapi.com/api/character/?gender=${gender}`;
+   let response = await fetch(url);
+   let data = await response.json();
+
+   res.render('results.ejs', { characters: data.results });
+});
+
+
+app.get('/catfacts', (req, res) => {
+    let fact = catFacts.random();
+    res.render('catfacts.ejs', { fact });
+});
+
+app.listen(3000, () => {
+   console.log('server started');
+});
